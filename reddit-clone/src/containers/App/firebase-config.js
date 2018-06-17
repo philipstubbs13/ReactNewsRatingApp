caf5a144b56fe5d529057fadbd4a3e0d52a24384
Phil S <philipstubbs13@gmail.com>
2018-06-17 15:12:17 -0500

    initialize firebase

---
+++ reddit-clone/src/containers/App/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase config file
+// Contains all the configs necessary to connect app with Firebase.
+
+export default {
+    apiKey: "AIzaSyDJIfRh8fCIVlZVtBTQm2xbzrsem4lglDo",
+    authDomain: "react-news-rating-app.firebaseapp.com",
+    databaseURL: "https://react-news-rating-app.firebaseio.com",
+    projectId: "react-news-rating-app",
+    storageBucket: "react-news-rating-app.appspot.com",
+    messagingSenderId: "744450513885"
+};
--- reddit-clone/src/containers/App/index.js
+++ reddit-clone/src/containers/App/index.js
@@ -1,10 +1,53 @@
+// import React
 import React, { Component } from 'react';
+// import Firebase
+import * as firebase from "firebase";
+// import Firebase 
+import config from './firebase-config';
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+
+  componentWillMount() {
+
+    //firebase.database() gives us a reference to the database service.
+    // Using ref(), we can get a specific reference from the database.
+    // if we we call ref('posts'), we'll be getting the posts reference from our database and storing that reference in postsRef.
+    let postsRef = firebase.database().ref('posts');
+
+    let _this = this;
+
+    // postsRef.on('value', ...) gives us the updated value whenever there's any change in the database.
+    // postsRef.once('value', ...) will only give us the data once.
+    postsRef.on('value', function(snapshot) {
+      console.log(snapshot.val());
+
+      // After we get the updated value in our on() callback, we store the values in our posts stae.
+      _this.setState({
+        posts: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
+  // Make the posts data available in all our children components, which will be passed through react-router.
+  // We're checking if this.props.childrn exists or not, and if it exists we clone that element and pass all our props to all our children.
+  // Calling cloneElement will shallowly merge the already existing props in this.props.children and the props we passed here (firebaseRef, posts, and loading).
+  // firebaseRef, posts, and loading props will be available to all routes.
   render() {
     return (
       <div className="App">
-        Hello World
+        {this.props.children && React.cloneElement(this.props.children, {
+          firebaseRef: firebase.database().ref('posts'),
+          posts: this.state.posts,
+          loading: this.state.loading 
+        })}
       </div>
     );
   }
