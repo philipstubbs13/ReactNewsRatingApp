25f32ece7f70bd01e0a5f6cc50f7f393e95ff88e
Phil S <philipstubbs13@gmail.com>
2018-06-20 19:53:51 -0500

    working on form

--- reddit-clone/src/containers/AddPost/index.js
+++ reddit-clone/src/containers/AddPost/index.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+});
 
 class AddPost extends Component {
   constructor() {
@@ -9,7 +28,8 @@ class AddPost extends Component {
   }
 
   state = {
-    title: ''
+    title: '',
+    body: ''
   };
 
   // The handleChange method updates our state with the value present in the input box. 
@@ -27,34 +47,46 @@ class AddPost extends Component {
     // Sets the current value of the title to our database.
     this.props.firebase.ref('posts').push({
       title: this.state.title,
+      body: this.state.body,
       upvote: 0,
       downvote: 0
     });
 
     // After the new post has been stored in the database, we make the input box empty again, ready to add a new post.
     this.setState({
-      title: ''
+      title: '',
+      body: ''
     });
   }
 
   render() {
+    const { classes } = this.props;
     return (
       <div className="AddPost main-content-section">
-        <input
-          type="text"
-          placeholder="Write the title of your post"
-          onChange={ this.handleChange }
-          value={ this.state.title }
-        />
-        <button
-          type="submit"
-          onClick={ this.handleSubmit }
-        >
-          Submit
-        </button>
+        <Paper className={classes.root} elevation={4}>
+          <Typography variant="headline" component="h1">
+            Add a post
+          </Typography>
+          <TextField
+            id="full-width"
+            label="Title"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            placeholder="Write the title of your post"
+            fullWidth
+            margin="normal"
+            onChange={ this.handleChange }
+            value={ this.state.title }
+          />
+          <Button variant="contained" color="primary" className={classes.button} type="submit"
+            onClick={ this.handleSubmit }>
+            Submit
+          </Button>
+        </Paper>
       </div>
     );
   }
 }
 
-export default AddPost;
+export default withStyles(styles)(AddPost);
