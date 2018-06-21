8d1eebd2767ae5570104b2f3f6084001a30930ec
Phil S <philipstubbs13@gmail.com>
2018-06-20 19:23:21 -0500

    got rid of hamburger menu and login button in app bar

--- reddit-clone/src/components/AppBar/AppBar.js
+++ reddit-clone/src/components/AppBar/AppBar.js
@@ -4,8 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 
 const styles = {
   root: {
@@ -26,13 +24,9 @@ function SwaggyAppBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <i className="fas fa-bars"></i>
-          </IconButton>
           <Typography variant="title" color="inherit" className={classes.flex}>
             React News Rating App
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
